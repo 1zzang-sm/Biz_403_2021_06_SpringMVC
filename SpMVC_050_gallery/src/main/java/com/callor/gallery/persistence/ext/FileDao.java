de7185732ab484f61326bc3a6fa06b235b5eb15d
1zzang-sm <captain1152@naver.com>
2021-07-06 15:51:07 +0900

    갤러리

---
+++ SpMVC_050_gallery/src/main/java/com/callor/gallery/persistence/ext/FileDao.java
@@ -0,0 +1,9 @@
+package com.callor.gallery.persistence.ext;
+
+import com.callor.gallery.model.FileDTO;
+import com.callor.gallery.persistence.GenericDao;
+
+public interface FileDao extends GenericDao<FileDTO, Long>{
+	
+	
+}
